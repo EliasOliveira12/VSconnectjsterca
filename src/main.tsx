7bb801131ab5df47982c04871d8092ca309630f4
Elias de Oliveira <134458921+EliasOliveira12@users.noreply.github.com>
2023-08-10 21:31:25 -0300

    terminando o site

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Route,Routes,BrowserRouter } from 'react-router-dom'
+import Footer from './components/Footer'
+import Header from './components/Header'
 // import App from './App'
 import './index.css'
-// import Home from './pages/Home'
+import Home from './pages/Home'
 import"./pages/pastalistadeservicos"
 import ListaServicos from './pages/pastalistadeservicos'
+// import ListaServicos from './pages/pastalistadeservicos'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    {/* <Home /> */}
-<ListaServicos />
+    <BrowserRouter>
+    <Header/>
+    <Routes>
+      <Route path='/'element={<Home/>}/>
+      <Route path='listaservicos'element={<ListaServicos/>}></Route>
+    </Routes>
+      <Footer/>
+    </BrowserRouter>
+
   </React.StrictMode>,
 )
